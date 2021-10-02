83d8c4ef347bd4e3c1a954bd9f6aee8ad334d70a
sdse95 <sdse95@gmail.com>
2021-10-02 09:58:36 -0500

    domotica
    archivos guardados

---
+++ public/firebase.js
@@ -0,0 +1,188 @@
+$(document).ready( function()
+ {
+    // Inicializar la base de datos
+    var config = {
+        apiKey: "AIzaSyCnNjZuL6XJoA4Ed9yXOn26cdEhrJyXxnQ",
+        authDomain: "dotronicdemo-4598f.firebaseapp.com",
+        databaseURL: "https://dotronicdemo-4598f-default-rtdb.firebaseio.com",
+        projectId: "dotronicdemo-4598f",
+        storageBucket: "dotronicdemo-4598f.appspot.com",
+        messagingSenderId: "224565645635"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+    var referencia=database.ref("home");
+
+    var value = false;
+    var value2 = false;
+    var value3 = false;
+    var value4 = false;
+    var value5 = false;
+    var value6 = false;
+
+    referencia.on("value", function(snapshot) {
+        var estado1 = (snapshot.val().luces.luz_sala);
+        var estado2 = (snapshot.val().luces.luz_sala2);
+        var estado3 = (snapshot.val().luces.luz_sala3);
+        var estado4 = (snapshot.val().luces.luz_sala_estar);
+        var estado5 = (snapshot.val().luces.luz_sala_estar2);
+        var estado6 = (snapshot.val().luces.luz_sala_estar3);
+
+        value = estado1;
+        value2 = estado2;
+        value3 = estado3;
+        value4 = estado4;
+        value5 = estado5;
+        value6 = estado6;
+
+        console.log(estado1);
+        console.log(estado2);
+        console.log(estado3);
+        console.log(estado4);
+        console.log(estado5);
+        console.log(estado6);
+
+        if (estado1) {
+            // algo si es true
+             document.getElementById('luz1').checked = true;
+          }else{
+            // algo si es false
+            document.getElementById('luz1').checked = false;
+        }
+
+        if (estado2) {
+            document.getElementById('luz2').checked = true;
+        }else{
+            document.getElementById('luz2').checked = false;
+        }
+
+        if (estado3) {
+            document.getElementById('luz3').checked = true;
+        }else{
+            document.getElementById('luz3').checked = false;
+        }
+        if (estado4) {
+            document.getElementById('luz1SalaDeEstar').checked = true;
+        }else{
+            document.getElementById('luz1SalaDeEstar').cheked = false;
+        }
+        if (estado5) {
+            document.getElementById('luz2SalaDeEstar').cheked = true;
+        }else{
+            document.getElementById('luz2SalaDeEstar').cheked = false;
+        }
+        if (estado6) {
+            document.getElementById('luz3SalaDeEstar').cheked = true;
+        }else {
+            document.getElementById('luz3SalaDeEstar').cheked = false;
+        }
+
+    });
+
+    var nombre = 'luces';
+    
+    $("#luz1").click(function(){
+        value=!value;
+        articulo = value;
+        referencia.child(nombre).set({
+        luz_sala: articulo,
+        luz_sala2: value2,
+        luz_sala3: value3,
+        luz_sala_estar: value4,
+        luz_sala_estar2: value5,
+        luz_sala_estar3:value6
+        });
+    });
+
+    var nombre = 'luces';
+    
+    $("#luz2").click(function(){
+        value2=!value2;
+        articulo = value2;  
+        referencia.child(nombre).set({
+        luz_sala2: articulo,
+        luz_sala: value,
+        luz_sala3: value3,
+        luz_sala_estar: value4,
+        luz_sala_estar2: value5,
+        luz_sala_estar3:value6
+        });
+    });
+
+    var nombre = 'luces';
+
+    $("#luz3").click(function(){
+        value3=!value3;
+        articulo = value3;  
+        referencia.child(nombre).set({
+        luz_sala2: value2,
+        luz_sala: value,
+        luz_sala3: articulo,
+        luz_sala_estar: value4,
+        luz_sala_estar2: value5,
+        luz_sala_estar3:value6
+        });
+    });
+
+    var nombre = 'luces';
+    $('#luz1SalaDeEstar').click(function(){
+        value4=!value4;
+        articulo = value4;
+        referencia.child(nombre).set({
+            luz_sala: value,
+            luz_sala2: value2,
+            luz_sala3: value3,
+            luz_sala_estar: articulo,
+            luz_sala_estar2: value5,
+            luz_sala_estar3:value6
+        });
+    });
+
+
+    var nombre = 'luces';
+    $('#luz2SalaDeEstar').click(function(){
+        value5=!value5;
+        articulo = value5;
+        referencia.child(nombre).set({
+            luz_sala: value,
+            luz_sala2: value2,
+            luz_sala3: value3,
+            luz_sala_estar: value4,
+            luz_sala_estar2: articulo,
+            luz_sala_estar3:value6
+        });
+    });
+
+    var nombre = 'luces';
+    $('#luz3SalaDeEstar').click(function(){
+        value6=!value6;
+        articulo = value6;
+        referencia.child(nombre).set({
+            luz_sala: value,
+            luz_sala2: value2,
+            luz_sala3: value3,
+            luz_sala_estar: value4,
+            luz_sala_estar2: value5,
+            luz_sala_estar3: articulo
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/index.php
@@ -0,0 +1,258 @@
+<!-- 
+* Copyright 2016 Carlos Eduardo Alfaro Orellana
+-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/sweetalert2.css">
+	<link rel="stylesheet" href="css/material.min.css">
+	<link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+	<link rel="stylesheet" href="css/main.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="js/material.min.js" ></script>
+	<script src="js/sweetalert2.min.js" ></script>
+	<script src="js/jquery.mCustomScrollbar.concat.min.js" ></script>
+	<script src="js/main.js" ></script>
+</head>
+
+<body>
+
+
+
+	<?php
+		$mysqli = new mysqli("localhost", "root","","cursos");
+			if ($mysqli->connect_errno) {
+				echo "Fallo al conectar MySQL: (" . $mysqli->connect_errno . ")". $mysqli->connect_error;
+			}
+			echo $mysqli->host_info . "\n";
+
+		$mysqli = new mysqli("127.0.0.1", "root", "", "cursos", 3306);
+			if ($mysqli->connect_errno) {
+    			echo "Fallo al conectar MySQL: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error:
+}
+
+echo $mysqli->host_info . "\n";
+	?>
+
+
+
+
+	<!-- navBar -->
+	<div class="full-width navBar">
+		<div class="full-width navBar-options">
+			<i class="zmdi zmdi-more-vert btn-menu" id="btn-menu"></i>	
+			<div class="mdl-tooltip" for="btn-menu">Menu</div>
+			<nav class="navBar-options-list">
+				<ul class="list-unstyle">
+
+					<li class="btn-exit" id="btn-exit">
+						<i class="zmdi zmdi-power"></i>
+						<div class="mdl-tooltip" for="btn-exit">LogOut</div>
+					</li>
+					<li class="text-condensedLight noLink" ><small>User Name</small></li>
+					<li class="noLink">
+						<figure>
+							<img src="assets/img/avatar-male.png" alt="Avatar" class="img-responsive">
+						</figure>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<!-- navLateral -->
+	<section class="full-width navLateral">
+		<div class="full-width navLateral-bg btn-menu"></div>
+		<div class="full-width navLateral-body">
+			<figure class="full-width" style="height: 77px;">
+				<div class="navLateral-body-cl">
+					<img src="assets/img/avatar-male.png" alt="Avatar" class="img-responsive">
+				</div>
+				<figcaption class="navLateral-body-cr hide-on-tablet">
+					<span>
+						Full Name Admin<br>
+						<small>Admin</small>
+					</span>
+				</figcaption>
+			</figure>
+			<nav class="full-width">
+				<ul class="full-width list-unstyle menu-principal">
+					<li class="full-width">
+						<a href="home.html" class="full-width">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-view-dashboard"></i>
+							</div>
+							<div class="navLateral-body-cr hide-on-tablet">
+								HOME
+							</div>
+						</a>
+					</li>
+					<li class="full-width divider-menu-h"></li>
+					<li class="full-width">
+						<a href="#!" class="full-width btn-subMenu">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-case"></i>
+							</div>
+							<div class="navLateral-body-cr hide-on-tablet">
+								ADMINISTRATION
+							</div>
+							<span class="zmdi zmdi-chevron-left"></span>
+						</a>
+						<ul class="full-width menu-principal sub-menu-options">
+							<li class="full-width">
+								<a href="company.html" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-balance"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										COMPANY
+									</div>
+								</a>
+							</li>
+							<li class="full-width">
+								<a href="providers.html" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-truck"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										PROVIDERS
+									</div>
+								</a>
+							</li>
+							<li class="full-width">
+								<a href="payments.html" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-card"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										PAYMENTS
+									</div>
+								</a>
+							</li>
+							<li class="full-width">
+								<a href="categories.html" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-label"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										CATEGORIES
+									</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+
+					<li class="full-width divider-menu-h"></li>
+					<li class="full-width">
+						<a href="#!" class="full-width btn-subMenu">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-wrench"></i>
+							</div>
+							<div class="navLateral-body-cr hide-on-tablet">
+								SETTINGS
+							</div>
+							<span class="zmdi zmdi-chevron-left"></span>
+						</a>
+						<ul class="full-width menu-principal sub-menu-options">
+							<li class="full-width">
+								<a href="#!" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-widgets"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										OPTION
+									</div>
+								</a>
+							</li>
+							<li class="full-width">
+								<a href="#!" class="full-width">
+									<div class="navLateral-body-cl">
+										<i class="zmdi zmdi-widgets"></i>
+									</div>
+									<div class="navLateral-body-cr hide-on-tablet">
+										OPTION
+									</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+	<!-- pageContent -->
+	<section class="full-width pageContent">
+		<section class="full-width text-center" style="padding: 40px 0;">
+			<h3 class="text-center tittles">HOME</h3>
+			<!-- Tiles -->
+			<article class="full-width tile">
+				<a href="cocina.html">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<br>
+						<small>Cocina</small>
+					</span>
+				</div>
+						</a>
+			</article>
+			<article class="full-width tile">
+				<a href="sala.html">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<br>
+						<small>Sala</small>
+					</span>
+				</div>
+			</a>
+			</article>
+			<article class="full-width tile">
+					<a href="saladeestar.html">
+					<div class="tile-text">
+						<span class="text-condensedLight">
+							<br>
+							<small>Sala De Estar</small>
+						</span>
+					</div>
+				</a>
+				</article>
+			<article class="full-width tile">
+					<a href="habitaciones.html">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<br>
+						<small>Habitaciones</small>
+					</span>
+				</div>
+			</a>
+			</article>
+			<article class="full-width tile">
+					<a href="baño.html">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<br>
+						<small>Baño</small>
+					</span>
+				</div>
+				<i class="fas fa-bath"></i>
+					</a>
+			</article>
+			<article class="full-width tile">
+					<a href="otros.html">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<br>
+						<small>Otros</small>
+					</span>
+				</div>
+					</a>
+			</article>
+			</article>
+		</section>
+	</section>
+</body>
+</html>
