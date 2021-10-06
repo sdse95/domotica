59c40882e5652714579cb64039c134550c868395
sdse95 <48522082+sdse95@users.noreply.github.com>
2021-10-06 15:08:40 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -3,8 +3,8 @@ const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const signupEmail = document.querySelector('#signup-email');
-	const signupPassword = document.querySelector('#signup-password');
+	const signupEmail = document.querySelector('#signup-email').value;
+	const signupPassword = document.querySelector('#signup-password').value;
 
 
 	console.log(signupEmail, signupPassword);
