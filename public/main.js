2e4c68e668fbde196e014b422cbb74abc5c09e81
sdse95 <48522082+sdse95@users.noreply.github.com>
2021-10-07 12:02:02 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,19 +1,48 @@
-const signupForm = document.querySelector('#signup-form');
+var firebaseConfig = {
+		    apiKey: "AIzaSyCnNjZuL6XJoA4Ed9yXOn26cdEhrJyXxnQ",
+		    authDomain: "dotronicdemo-4598f.firebaseapp.com",
+		    databaseURL: "https://dotronicdemo-4598f-default-rtdb.firebaseio.com",
+		    projectId: "dotronicdemo-4598f",
+		    storageBucket: "dotronicdemo-4598f.appspot.com",
+		    messagingSenderId: "224565645635",
+		    appId: "1:224565645635:web:3c73c635e291c0d36d1ff6",
+		    measurementId: "G-WQGZDD28YH"
+		  };
 
-signupForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+		  // Initialize Firebase
 
-	const email = document.querySelector('#signup-email').value;
-	const password = document.querySelector('#signup-password').value;
+		  firebase.initializeApp(firebaseConfig);
+		  
+		  const auth = firebase.auth();
 
-	auth
-		.createUserWithEmailAndPassword(email,password)
-		.then(userCredential => {
-			console.log('sing up')
-		})
-		.catch((error) => {	
-		    var errorCode = error.code;
-		    var errorMessage = error.message;
-		    console.log(errorCode, errorMessage)
-  		});
-})
+		  //signup function
+		  function signUp(){
+		  	var email = document.getElementById("email");
+		  	var password = document.getElementById("password");
+
+		  	const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+
+		  	promise.cath(e=> alert(e.message));
+		  	aaler("SingUp Succesfully");
+		  }
+
+
+		  //singIn function
+
+		  function signOut(){
+		  	auth.signOut();
+		  	alert("signOut Successfully from System");
+		  }
+
+
+		  //activate user to homepage
+		  firebase.auth().onAuthStateChanged((User)=>{
+		  	if(user){
+		  		var email = user.email;
+
+		  		alert("Activate user "+email);
+		  	}else{
+		  		alert("No Activate User Found")
+		  	}
+
+		  })
