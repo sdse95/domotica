606423ced7aa5f8007bce814a567104e449d79d1
sdse95 <48522082+sdse95@users.noreply.github.com>
2021-10-07 17:17:42 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -38,6 +38,29 @@ $(document).ready(function(){
 		},
 		function(isConfirm) {
 		  	if (isConfirm) {
+                var firebaseConfig = {
+                    apiKey: "AIzaSyCnNjZuL6XJoA4Ed9yXOn26cdEhrJyXxnQ",
+                    authDomain: "dotronicdemo-4598f.firebaseapp.com",
+                    databaseURL: "https://dotronicdemo-4598f-default-rtdb.firebaseio.com",
+                    projectId: "dotronicdemo-4598f",
+                    storageBucket: "dotronicdemo-4598f.appspot.com",
+                    messagingSenderId: "224565645635",
+                    appId: "1:224565645635:web:3c73c635e291c0d36d1ff6",
+                    measurementId: "G-WQGZDD28YH"
+                  };
+
+          // Initialize Firebase
+
+              firebase.initializeApp(firebaseConfig);
+              
+              const auth = firebase.auth();
+              //singOut function
+
+            function signOut(){
+               auth.signOut();
+               alert("signOut Successfully from System");
+            }
+
 		    	window.location='index.html'; 
 		  	}
 		});
