613260c7aa5f86e988d89dba2bbd96487f179a55
sdse95 <48522082+sdse95@users.noreply.github.com>
2021-10-07 17:08:46 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -33,7 +33,7 @@ var firebaseConfig = {
 		  	var password = document.getElementById("password");
 		  	const promise = auth.signInWithEmailAndPassword(email.value, password.value);
 		  	promise.catch(e=>alert(e.message));
-		  	window.location.href="home.html"
+		  	window.location.href="https://dotronicdemo-4598f.firebaseapp.com/home.html"
 		  }
 
 
